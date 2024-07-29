9753a95f8d6a12c90433d083dba55699d0e2b643
jinwoong16 <baseball0020@gmail.com>
2024-07-29 21:54:36 +0900

    Initial Commit

---
+++ ConflictExample/ConflictExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConflictExampleApp.swift
+//  ConflictExample
+//
+//  Created by jinwoong Kim on 7/29/24.
+//
+
+import SwiftUI
+
+@main
+struct ConflictExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
